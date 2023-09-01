50638c7a37403e4ba9474df1d70261432673814d
Ghermany Paganini <ghermanypaganini@gmail.com>
2023-09-01 12:29:05 +0000

    teste 01

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-function Home(){
-    return <h1> Oi Tereza! Divertido? </h1>
-}
-   
+    const title = document.createElement('h1');
+    title.textContent = 'Olá Tereza!';
 
-export default Home;
+    const text = document.createElement('p');
+    text.textContent = 'Este é um exemplo de uma página simples com um título, um texto e uma imagem.';
+
+    const image = document.createElement('img');
+    image.src = 'https://png.pngtree.com/png-vector/20220122/ourlarge/pngtree-lettering-i-love-you-png-image_226175.png';
+
+
+    document.body.appendChild(title);
+    document.body.appendChild(text);
+    document.body.appendChild(image);
